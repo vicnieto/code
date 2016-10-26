35505d30e9ce45709a4ab29750dd4c5224cf9a0a
victornieto <vnieto@ucsc.edu>
2016-10-25 20:08:51 -0700

    added code2040 api file so I can do challenge

---
+++ code2040Api.py
@@ -0,0 +1,9 @@
+# code 2040 challenge
+import requests
+import json
+
+payload={"token" : "47098a3d5718c1178509ed5d7f1efde2", "github" : "http://challenge.code2040.org/api/register"}
+
+requests = requests.post(url, json=payload)
+
+instructions.text 
